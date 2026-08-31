038dc0d969b252ecfe6e2b5cd23f29ed8d7988b7
Felipe Domingues <felipedominguessousa@gmail.com>
2026-08-31 13:33:52 -0300

    migração de css para scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './styles/globals.css';
+import './styles/main.scss';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
